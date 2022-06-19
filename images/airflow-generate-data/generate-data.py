fd7e61fcbd0fe47fc00e3b3b5a77163dd7efbe78
d3vyatk4ru <danya.devyatkin@mail.ru>
2022-06-19 18:25:10 +0300

    some modified

--- images/airflow-generate-data/generate-data.py
+++ images/airflow-generate-data/generate-data.py
@@ -8,7 +8,7 @@ from sklearn.datasets import load_breast_cancer
 
 
 @click.command("generate")
-@click.option("--output-path")
+@click.option("--output_path")
 def generate_data_cancer(output_path: str) -> NoReturn:
     """ func for data generation """
     n_entries = np.random.randint(10, 568)
@@ -16,8 +16,8 @@ def generate_data_cancer(output_path: str) -> NoReturn:
 
     os.makedirs(output_path, exist_ok=True)
 
-    X[:n_entries].to_csv(os.path.join(output_path, "data.csv"))
-    y[:n_entries].to_csv(os.path.join(output_path, "target.csv"))
+    X[:n_entries].to_csv(os.path.join(output_path, 'data.csv'))
+    y[:n_entries].to_csv(os.path.join(output_path, 'target.csv'))
 
 
 if __name__ == '__main__':
